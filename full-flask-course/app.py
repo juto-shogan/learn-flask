552c858bc3366e62def330aaef267f109eb33c19
juto-shogan <somtombonu53@gmail.com>
2025-06-12 21:56:37 +0100

    progress set one

--- full-flask-course/app.py
+++ full-flask-course/app.py
@@ -3,10 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return "<h1>Hello, World!</h1>"
 
+@app.route('/hello')
+def hello():
+    return 'hello world'
 
+@app.route('/greet/<name>')
+def greet(name):
+    return f'hello {name}'
+
+@app.route('/add/<int:a>/<int:b>')
+def add(a, b):
+    return f'The sum of {a} and {b} is {a + b}'
+``
 if  __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
