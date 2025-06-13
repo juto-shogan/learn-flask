7ad8f1d1752dadd739880db112bb187ea66d6dc9
juto-shogan <somtombonnu53@gmail.com>
2025-06-13 12:34:26 +0100

    routes & urls

--- full-flask-course/app.py
+++ full-flask-course/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -6,10 +6,13 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello, World!</h1>"
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    return 'hello world'
-
+    response = make_response('hello world')
+    response.status_code = 202
+    response.headers['Content-Type'] = 'application/octet-stream'
+    return response
+    
 @app.route('/greet/<name>')
 def greet(name):
     return f'hello {name}'
@@ -17,6 +20,15 @@ def greet(name):
 @app.route('/add/<int:a>/<int:b>')
 def add(a, b):
     return f'The sum of {a} and {b} is {a + b}'
-``
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and name in request.args.keys():
+        greeting = request.args['greeting']
+        name = request.args.get('name')
+        return f'{greeting}, {name}!'
+    else:
+        return 'Missing parameters! Please provide both greeting and name.'
+
 if  __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
