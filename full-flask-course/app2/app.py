4493e57787a164b54dcc4b8a8278e83ecd675b2f
juto-shogan <somtombonnu53@gmail.com>
2025-06-18 10:19:04 +0100

    testing API

--- full-flask-course/app2/app.py
+++ full-flask-course/app2/app.py
@@ -1,17 +1,33 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {'tim': {'age': 25, 'gender': 'male'}, 
-         'bob': {'age': 30, 'gender': 'male'}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 
-class HelloWorld(Resource):
-    def get(self, name, test):
-        return {'data': name, 'test': test}
+videos = {}
 
-api.add_resource(HelloWorld, '/helloworld/<string:name>/')
+def aborter_if_vid_doesnt_exist(video_id):
+    if video_id not in videos: 
+        abort(404, 'video does not exist')
+
+class Video(Resource):
+    def get(self, video_id):
+        aborter_if_vid_doesnt_exist(video_id)
+        return videos[video_id]
+    
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+
+api.add_resource(Video, '/video/<string:video_id>')
+                                                                                      
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001, host='127.0.0.1')    
--- full-flask-course/app2/test.py
+++ full-flask-course/app2/test.py
@@ -1,6 +1,11 @@
 import requests
 
-BASE = 'http://127.0.0.1:5000/'
+BASE = 'http://127.0.0.1:5001/'
 
-response = requests.get(BASE + 'helloworld/tim/')
+# response = requests.put(BASE + 'video/1', {'likes':10, 'name':'Video 1', 'views':10000})
+# print(response.json())
+
+# input()
+
+response = requests.get(BASE + 'video/6')
 print(response.json())
